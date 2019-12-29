277707d8fd0e8c3e7779dfab483180ee0f026495
laaaaanceeee <ilance.mojal@gmail.com>
2019-12-29 23:04:37 +0800

    'night fix'

--- src/app/pages/questions/questions.page.ts
+++ src/app/pages/questions/questions.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { AccessProviders } from 'src/app/providors/access-providers';
+import { ToastController, LoadingController } from '@ionic/angular';
 
 
 
@@ -11,8 +13,14 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class QuestionsPage implements OnInit {
 
   hasCode:boolean = false;
+  codeTyped:string = "";
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private toastCtrl: ToastController,
+    private loadingCtrl: LoadingController,
+    private accsPrvds: AccessProviders
+    ) { }
 
   ngOnInit() {
   }
@@ -20,4 +28,52 @@ export class QuestionsPage implements OnInit {
   showQuestion(){
     this.hasCode = true;
   }
+  async getQuestion(){
+    if(this.codeTyped == ""){
+      this.presentToast('Code is required.');
+    } else {
+      console.log(this.codeTyped);
+      return new Promise(resolve => {
+        let body = {
+          aksi: 'get_question',
+          code: this.codeTyped
+          // email: this.email,
+          // password: this.password
+        }
+  
+        this.accsPrvds.postData(body, 'proses_api.php').subscribe((res:any)=>{
+          console.log(res.success);
+          console.log(res.result);
+          if(res.success==true){
+         
+            this.showQuestion();
+            // this.presentToast('Login successfuly');
+            // this.storage.set('storage_xxx', res.result); // create storage session
+            // this.navCtrl.navigateRoot(['/home']);
+          }else{
+         
+            this.presentToast('Code incorrect');
+            // this.presentToastWithOptions('Email or password is incorrect');
+  
+          }
+  
+        },(err)=>{
+          
+          
+            this.presentToast('Timeout');
+            console.log(err)
+        });
+      });
+    }
+
+
+   }
+
+   async presentToast(a){
+    const toast = await this.toastCtrl.create({
+      message:a,
+      duration: 1500,
+    });
+    toast.present();
+    }
 }
