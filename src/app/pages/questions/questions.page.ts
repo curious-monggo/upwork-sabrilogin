d066461bfbf4b30be2c970e013fa842013ce6488
laaaaanceeee <ilance.mojal@gmail.com>
2020-01-01 14:12:39 +0800

    'questions-page, done adding comments'

--- src/app/pages/questions/questions.page.ts
+++ src/app/pages/questions/questions.page.ts
@@ -17,10 +17,29 @@ export class QuestionsPage implements OnInit {
   datastorage: any;
   user_id: string;
 
-  hasCode: boolean = false;
-  showVideoCard: boolean = false;
+  /**
+    Boolean values that will help show/hide the cards.
+    Syntax: *ngIf="boolVar; else #nameOfNgTemplate"
+    We used it in questions.page.html
+    see link: https://ultimatecourses.com/blog/angular-ngif-else-then
+  **/
+
+  questionCardIsVisible: boolean = false;
+  videoCardIsVisible: boolean = false;
+
+  //boolean for [disabled] attr
+  answerDisabled: boolean = false;
+
   codeTyped: string = "";
+
+  //We use an array because it can handle multiple values
   choices: any = [];
+
+  /**
+    Typescript objects, it's a useful way of structuring data.
+    When we use it in html, we --> {{question.img_url}}
+    see link: https://www.tutorialspoint.com/typescript/typescript_objects.htm
+  **/
   question = {
     question_id: "",
     img_url: "",
@@ -28,7 +47,10 @@ export class QuestionsPage implements OnInit {
     text: "",
     created_at: ""
   };
-  answerDisabled: boolean = false;
+
+
+  //the selected answer of the user,
+  //we get the id to be saved to the database as reference
   chosen_choice_id: string;
 
   constructor(
@@ -41,14 +63,15 @@ export class QuestionsPage implements OnInit {
   ) {}
 
   ngOnInit() {}
+
   ionViewDidEnter() {
     this.storage.get("storage_xxx").then(res => {
       this.datastorage = res;
       this.user_id = this.datastorage.id_user;
     });
   }
-  showQuestionCard() {
-    this.showVideoCard = false;
+  hideVideoCard() {
+    this.videoCardIsVisible = false;
   }
   async getQuestion() {
     if (this.codeTyped == "") {
@@ -62,6 +85,7 @@ export class QuestionsPage implements OnInit {
       loader.present();
 
       return new Promise(resolve => {
+        //We pass the process to be done to fetch question by id
         let body = {
           aksi: "get_question",
           code: this.codeTyped,
@@ -75,15 +99,18 @@ export class QuestionsPage implements OnInit {
             if (res.success == true) {
               loader.dismiss();
 
+              //I save the result to the question object
               this.question.img_url = res.result.question.img_url;
               this.question.video_url = res.result.question.video_url;
               if (this.question.video_url) {
-                this.showVideoCard = true;
+                this.videoCardIsVisible = true;
               }
               this.question.text = res.result.question.text;
               this.question.question_id = res.result.question.question_id;
               this.choices = res.result.choices;
-              this.hasCode = true;
+
+              //then display
+              this.questionCardIsVisible = true;
             } else {
               loader.dismiss();
               this.presentToast("Code may already be used or incorrect.");
@@ -111,11 +138,13 @@ export class QuestionsPage implements OnInit {
     console.log(this.chosen_choice_id);
     this.tryAnswer();
   }
-
+  //same model of code, only different process. Here we try to save the answer
   async tryAnswer() {
     if (this.chosen_choice_id == "" || this.chosen_choice_id == undefined) {
       this.presentToast("Please choose an answer");
     } else {
+      //while the process runs, we disable the button to prevent another process to run
+      //it may cause conflict or even replace previous data sent. 
       this.answerDisabled = true;
 
       const loader = await this.loadingCtrl.create({
@@ -139,6 +168,7 @@ export class QuestionsPage implements OnInit {
             } else {
               loader.dismiss();
               this.presentToast(res.msg);
+              //The server returns a false, we allow the user to try again
               this.answerDisabled = false;
             }
           },
@@ -151,7 +181,7 @@ export class QuestionsPage implements OnInit {
       });
     }
   }
-
+  //I modifed it so that when user presses ok on the alert, it pops back to home.
   async presentAlert(header, subheader, msg) {
     const alert = await this.alertCtrl.create({
       header: header,
